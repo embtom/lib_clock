95cc3c746a83bddc295072f3fca31e5b1af9abc4
thomas <t.willetal@gmail.com>
2018-03-19 20:19:34 +0100

    Timer is used modul time calculation

--- lib_clock.c
+++ lib_clock.c
@@ -37,9 +37,40 @@
 	#error No Architecture is set at lib_clock
 #endif
 
+/* *******************************************************************
+ * defines
+ * ******************************************************************/
 
-static TIM_HandleTypeDef s_lib_delay__tim_hdl;
-uint16_t Ftim_MHz;		// timer speed in MHz
+#define JF_TIM_TIMER		  TIM4;
+#define JF_MAX_TIM_VALUE      (0xFFFF)    // 16bit counters
+
+/* *******************************************************************
+ * custom data types (e.g. enumerations, structures, unions)
+ * ******************************************************************/
+typedef int32_t   jiffy_t;    // Jiffy type 4 byte integer
+typedef TIM_HandleTypeDef jf_timer_t;
+
+typedef volatile struct {
+	jf_timer_t	timer_hdl;
+	jiffy_t		*value;        // Pointer to timers current value
+	uint32_t    freq;          // timer's  frequency
+	uint32_t    jiffies;       // jiffies max value (timer's max value)
+	jiffy_t     jpus;          // Variable for the delay function
+}jf_t;
+
+/* *******************************************************************
+ * static function declarations
+ * ******************************************************************/
+static int jf_check_usec (int32_t _usec);
+static jiffy_t jf_per_usec (jf_t *_jf);
+static int JF_setfreq (jf_t *_jf, uint32_t _jf_freq, uint32_t _jiffies);
+
+/* *******************************************************************
+ * (static) variables declarations
+ * ******************************************************************/
+static jf_t s_jf;
+static unsigned int s_milliseconds_ticks;
+static uint64_t s_milliseconds_ticks_64bits;
 
 /* ************************************************************************//**
  * \brief	Initialization of the timing module
@@ -51,50 +82,9 @@ uint16_t Ftim_MHz;		// timer speed in MHz
  * ****************************************************************************/
 int lib_clock__init(void)
 {
-	TIM_Base_InitTypeDef init_arg;	// universal temporary init structure for timer configuration
-	int Ftim6_Hz;
-
-	/* configure hardware timer 6 */
-
-	/* TIM4 clock enable */
-	__HAL_RCC_TIM4_CLK_ENABLE();
-
-	/* setup Timer 4 for counting mode */
-	/* Time Base configuration */
-	init_arg.Prescaler 			= 65535;										// Specifies the prescaler value used to divide the TIM clock. (0 = div by 1) This parameter can be a number between 0x0000 and 0xFFFF
-	init_arg.CounterMode 		= TIM_COUNTERMODE_UP;
-	init_arg.Period 			= 4000;						// Auto reload register (upcounting mode => reset cnt when value is hit, and throw an overflow interrupt)
-	init_arg.ClockDivision 		= TIM_CLOCKDIVISION_DIV1;		// not available for TIM6 and 7 => will be ignored
-	init_arg.RepetitionCounter 	= 0;							// start with 0 again after overflow
-
-	s_lib_delay__tim_hdl.Init = init_arg;
-	s_lib_delay__tim_hdl.Instance = TIM4;
-
-	HAL_TIM_Base_Init(&s_lib_delay__tim_hdl);
-
-	/* TIM6 counter enable (free running) */
-	__HAL_TIM_ENABLE(&s_lib_delay__tim_hdl);
-	__HAL_TIM_ENABLE_IT(&s_lib_delay__tim_hdl, TIM_IT_UPDATE);
-
-	// TIM4->DIER = TIM_DIER_UIE;
-
-	// clock tree: SYSCLK --AHBprescaler--> HCLK --APB1prescaler--> PCLK1 --TIM6multiplier--> to TIM 2,3,4,6,7
-	// clock tim6: Input=PCLK1 (APB1 clock) (multiplied x2 in case of APB1 clock divider > 1 !!! (RCC_CFGR.PRE1[10:8].msb[10] = 1))
-	if (RCC->CFGR & RCC_CFGR_PPRE1_2)
-		Ftim6_Hz = HAL_RCC_GetPCLK1Freq() * 2;
-	else
-		Ftim6_Hz = HAL_RCC_GetPCLK2Freq();
-
-	// Timer internal prescaler
-	Ftim6_Hz /= ((TIM4->PSC) + 1);
-
-	// set info global available
-	Ftim_MHz = Ftim6_Hz / 1000000;
-
-
-	NVIC_EnableIRQ(TIM4_IRQn); // Enable TIM16 IRQ
-
-
+	s_milliseconds_ticks_64bits = 0;
+	s_milliseconds_ticks = 0;
+	jf_init(&s_jf,1000000, 1000);  // 1MHz timer, 1000 counts, 1 usec per count
 	return EOK;
 }
 
@@ -110,7 +100,7 @@ int lib_clock__init(void)
  * ****************************************************************************/
 uint32_t lib_clock__get_time_ms(void)
 {
-	return HAL_GetTick();
+	return s_milliseconds_ticks;
 }
 
 /* ************************************************************************//**
@@ -125,7 +115,13 @@ uint32_t lib_clock__get_time_ms(void)
  * ****************************************************************************/
 uint64_t lib_clock__get_time_ns(void)
 {
+	uint64_t ns;
 
+	ns = s_milliseconds_ticks_64bits * 1000000ULL;
+	if (s_jf.value) {
+		ns += (uint64_t)s_jf.value * 1000ULL;
+	}
+	return ns;
 }
 
 /* ************************************************************************//**
@@ -140,6 +136,13 @@ uint64_t lib_clock__get_time_ns(void)
  * ****************************************************************************/
 uint64_t lib_clock__get_time_us(void)
 {
+	uint64_t us;
+
+	us = s_milliseconds_ticks_64bits * 1000ULL;
+	if (s_jf.value) {
+		us += (uint64_t)s_jf.value;
+	}
+	return us;
 
 }
 
@@ -169,14 +172,14 @@ uint64_t lib_clock__get_time_us(void)
  * ****************************************************************************/
 uint32_t lib_clock__get_time_since_ms(uint32_t _lasttime)
 {
-
+	return (s_milliseconds_ticks - _lasttime);
 }
 
 /* ************************************************************************//**
  * \brief delay the calling thread for the given amount of microseconds
  *
  * This function delays the calling thread for _delay microseconds.
- * On OSEK, this function implements a busy wait with an accuracy of 1us.
+ * This function implements a busy wait with an accuracy of 1us.
  * On HOSes, this function implements a thread suspension with an implementation
  * dependent accuracy between 1us and several milliseconds. Furthermore, this
  * function may get prematurely interrupted by a signal on HOSes.
@@ -185,7 +188,19 @@ uint32_t lib_clock__get_time_since_ms(uint32_t _lasttime)
  * ****************************************************************************/
 void lib_clock__delay_us(uint32_t _delay)
 {
+	jiffy_t m, m2, m1 = *s_jf.value;
 
+	_delay *= s_jf.jpus;
+	if (*s_jf.value - m1 > _delay) // Very small delays will return here.
+		return;
+
+	// Delay loop: Eat the time difference from usec value.
+	while (_delay > 0) {
+		m2 = *s_jf.value;
+	    m = m2 - m1;
+	    _delay -= (m>0) ? m : s_jf.jiffies + m;
+	    m1 = m2;
+	}
 }
 
 /* ************************************************************************//**
@@ -205,14 +220,118 @@ uint64_t lib_clock__get_clock_ticks(void)
 }
 
 
+// Initialise the jf to a desired jiffy frequency f
+int jf_init (jf_t *_jf, uint32_t _jf_freq, uint32_t _jiffies)
+{
+	int ret;
+	ret = JF_setfreq (_jf,_jf_freq, _jiffies);
+    if (ret < EOK) {
+    	return ret;
+    }
+    _jf->jiffies = _jiffies;
+    _jf->freq = _jf_freq;
+    _jf->jpus = jf_per_usec (_jf);
+   return EOK;
+}
+
+
+/*!
+ * \brief
+ *    A code based polling version delay implementation, using jiffies for timing.
+ *    This is NOT accurate but it ensures that the time passed is always
+ *    more than the requested value.
+ *    The delay values are multiplications of 1 usec.
+ * \param
+ *    usec     Time in usec for delay
+ */
+ static int jf_check_usec (int32_t _usec)
+ {
+   static jiffy_t m1=-1, cnt;
+   jiffy_t m, m2;
+
+   if (m1 == -1) {
+      m1 = *s_jf.value;
+      cnt = s_jf.jpus * _usec;
+   }
+
+   if (cnt>0) {
+      m2 = *s_jf.value;
+      m = m2-m1;
+      cnt-= (m>0) ? m : s_jf.jiffies + m;
+      m1 = m2;
+      return 1;   // wait
+   }
+   else {
+      m1 = -1;
+      return 0;   // do not wait any more
+   }
+}
+
+ // Return the systems best approximation for jiffies per usec
+ static jiffy_t jf_per_usec (jf_t *_jf)
+ {
+    jiffy_t jf = _jf->freq / 1000000;
+
+    if (jf <= _jf->jiffies)
+       return jf;
+    else
+       // We can not count beyond timer's reload
+       return 0;
+ }
+
+/*
+ * Time base configuration using the TIM7
+ * \param jf_freq  The TIMER's frequency
+ * \param jiffies  The TIMER's max count value
+ */
+static int JF_setfreq (jf_t *_jf, uint32_t _jf_freq, uint32_t _jiffies)
+{
+	TIM_Base_InitTypeDef init_arg;	// universal temporary init structure for timer configuration
+	int Ftim_Hz;
+
+	/* TIM4 clock enable */
+	__HAL_RCC_TIM4_CLK_ENABLE();
+
+		// clock tree: SYSCLK --AHBprescaler--> HCLK --APB1prescaler--> PCLK1 --TIM6multiplier--> to TIM 2,3,4,6,7
+		// clock tim6: Input=PCLK1 (APB1 clock) (multiplied x2 in case of APB1 clock divider > 1 !!! (RCC_CFGR.PRE1[10:8].msb[10] = 1))
+	if (RCC->CFGR & RCC_CFGR_PPRE1_2)
+		Ftim_Hz = HAL_RCC_GetPCLK1Freq() * 2;
+	else
+		Ftim_Hz = HAL_RCC_GetPCLK2Freq();
+
+	/* setup Timer 4 for counting mode */
+	/* Time Base configuration */
+	init_arg.Prescaler 			= Ftim_Hz /_jf_freq ;										// Specifies the prescaler value used to divide the TIM clock. (0 = div by 1) This parameter can be a number between 0x0000 and 0xFFFF
+	init_arg.CounterMode 		= TIM_COUNTERMODE_UP;
+	init_arg.Period 			= _jiffies & JF_MAX_TIM_VALUE;						// Auto reload register (upcounting mode => reset cnt when value is hit, and throw an overflow interrupt)
+	init_arg.ClockDivision 		= TIM_CLOCKDIVISION_DIV1;		// not available for TIM6 and 7 => will be ignored
+	init_arg.RepetitionCounter 	= 0;							// start with 0 again after overflow
+
+	_jf->timer_hdl.Init = init_arg;
+	_jf->timer_hdl.Instance = JF_TIM_TIMER;
+
+	HAL_TIM_Base_Init(&_jf->timer_hdl);
+	_jf->value = &_jf->timer_hdl.Instance->CNT;
+
+	__HAL_TIM_ENABLE(&_jf->timer_hdl);
+	__HAL_TIM_ENABLE_IT(&_jf->timer_hdl, TIM_IT_UPDATE);
+
+	// Timer internal prescaler
+	Ftim_Hz /= ((TIM4->PSC) + 1);
+
+	NVIC_EnableIRQ(TIM4_IRQn); // Enable TIM16 IRQ
+	return EOK;
+}
+
 void TIM4_IRQHandler (void)
 {
-	static unsigned int tim4_count = 0;
+	unsigned int tick_time = 0;
 
-	 __HAL_TIM_CLEAR_FLAG(&s_lib_delay__tim_hdl, TIM_IT_UPDATE);
-
-//	TIM4->SR = 0; //~(TIM_IT_UPDATE);
-	tim4_count ++;
-
+	if (s_jf.freq) {
+		tick_time = (s_jf.jiffies * 1000.0)/s_jf.freq;
+	}
+	s_milliseconds_ticks += tick_time;
+	s_milliseconds_ticks_64bits += tick_time;
+	 __HAL_TIM_CLEAR_FLAG(&s_jf.timer_hdl, TIM_IT_UPDATE);
 }
 
