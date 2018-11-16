8ff8564e5d5cc0a5e5f01a48d4998f92ac012b8a
Thomas Willetal <t.willetal@googlemail.com>
2018-11-17 00:25:52 +0100

    fix comments

--- include/lib_clock.h
+++ include/lib_clock.h
@@ -47,10 +47,8 @@ int lib_clock__init(void);
 /* ************************************************************************//**
  * \brief	get current timestamp in milliseconds
  *
- * This function returns a monotonic timestamp with no relation to the local
- * time and date. The returned value overflows after approximately 49.7 days.
- * The returned value may hence very well be 0. Yet, if every call returns 0,
- * the implementation is likely to be improperly supported by the underlying OS.
+ * This function returns a monotonic timestamp.
+ * The returned value overflows after approximately 49.7 days.
  *
  * \return	current timestamp in milliseconds
  * ****************************************************************************/
@@ -59,11 +57,8 @@ uint32_t lib_clock__get_time_ms(void);
 /* ************************************************************************//**
  * \brief	get current timestamp in nanoseconds
  *
- * This function returns a monotonic timestamp with no relation to the local
- * time and date. The returned value overflows after approximately 585 years.
- * Note that the underlying timer's resolution is OS dependent and is usually
- * in the dimension of nanoseconds.
- *
+ * This function returns a monotonic timestamp 
+ * 
  * \return	current timestamp in nanoseconds
  * ****************************************************************************/
 uint64_t lib_clock__get_time_ns(void);
@@ -71,11 +66,7 @@ uint64_t lib_clock__get_time_ns(void);
 /* ************************************************************************//**
  * \brief	get current timestamp in microseconds
  *
- * This function returns a monotonic timestamp with no relation to the local
- * time and date. The returned value overflows after approximately 585 years.
- * Note that the underlying timer's resolution is OS dependent and is usually
- * in the dimension of microseconds.
- *
+
  * \return	current timestamp in microseconds
  * ****************************************************************************/
 uint64_t lib_clock__get_time_us(void);
@@ -83,37 +74,13 @@ uint64_t lib_clock__get_time_us(void);
 /* ************************************************************************//**
  * \brief	get relative time difference since the given timestamp in milliseconds
  *
- * The returned value may be 0 in the following cases:
- *	1.	The difference really is 0
- *		In that case, one should verify whether this function provides a
- *		sufficient time resolution under all possible circumstances.
- *	2.	The time difference is 49.7 days
- *		In that case, one should verify whether this function provides a
- *		sufficient value range under all possible circumstances.
- *	3.	An error has occurred (provided that _lasttime is also 0)
- *		This may actually only happen if this function is likely to be not
- *		properly supported by the underlying OS.
- *
- * WARNING:
- *	1.	If the actual time difference is greater than 49.7 days, the value of
- *		the difference will overflow, providing an incorrect result.
- *	2.	If _lasttime was provided by a different function than
- *		lib_module_time__get_time(), the result is likely to be invalid, as
- *		the utilized time bases may not be the same.
- *
  * \param	_lasttime			timestamp to calculate the difference from
  * \return	relative time difference in milliseconds
  * ****************************************************************************/
 uint32_t lib_clock__get_time_since_ms(uint32_t _lasttime);
 
 /* ************************************************************************//**
- * \brief delay the calling thread for the given amount of microseconds
- *
- * This function delays the calling thread for _delay microseconds.
- * On OSEK, this function implements a busy wait with an accuracy of 1us.
- * On HOSes, this function implements a thread suspension with an implementation
- * dependent accuracy between 1us and several milliseconds. Furthermore, this
- * function may get prematurely interrupted by a signal on HOSes.
+ * \brief blocking delay 
  *
  *  \param _delay : number of microseconds to delay the caller
  * ****************************************************************************/
@@ -121,12 +88,6 @@ void lib_clock__delay_us(uint32_t _delay);
 
 /* ************************************************************************//**
  * \brief	get clock ticks since system startup
- *
- * The CPU time base will overrun after several dozens to several 1000 years,
- * depending on the clock resolution, e.g.:
- *	4679a @  80ns (= 125MHz system clock)
- *	  58a @ 100ps (=  10GHz system clock)
- * Therefore an overrun handling is not really necessary.
  *
  * \return	number of clock ticks as 64bit value
  * ****************************************************************************/
--- src/stm32/lib_clock_STM32.c
+++ src/stm32/lib_clock_STM32.c
@@ -15,7 +15,6 @@
  *  
  */
 
-/* https://stackoverflow.com/questions/29010847/how-to-make-a-microsecond-precise-timer-on-the-stm32l-discovery-arm-board*/
 
 /* *******************************************************************
  * includes
@@ -112,10 +111,8 @@ int lib_clock__init(void)
 /* ************************************************************************//**
  * \brief	get current timestamp in milliseconds
  *
- * This function returns a monotonic timestamp with no relation to the local
- * time and date. The returned value overflows after approximately 49.7 days.
- * The returned value may hence very well be 0. Yet, if every call returns 0,
- * the implementation is likely to be improperly supported by the underlying OS.
+ * This function returns a monotonic timestamp 
+ * The returned value overflows after approximately 49.7 days.
  *
  * \return	current timestamp in milliseconds
  * ****************************************************************************/
@@ -126,11 +123,6 @@ uint32_t lib_clock__get_time_ms(void)
 
 /* ************************************************************************//**
  * \brief	get current timestamp in nanoseconds
- *
- * This function returns a monotonic timestamp with no relation to the local
- * time and date. The returned value overflows after approximately 585 years.
- * Note that the underlying timer's resolution is OS dependent and is usually
- * in the dimension of nanoseconds.
  *
  * \return	current timestamp in nanoseconds
  * ****************************************************************************/
@@ -151,11 +143,6 @@ uint64_t lib_clock__get_time_ns(void)
 /* ************************************************************************//**
  * \brief	get current timestamp in microseconds
  *
- * This function returns a monotonic timestamp with no relation to the local
- * time and date. The returned value overflows after approximately 585 years.
- * Note that the underlying timer's resolution is OS dependent and is usually
- * in the dimension of microseconds.
- *
  * \return	current timestamp in microseconds
  * ****************************************************************************/
 uint64_t lib_clock__get_time_us(void)
@@ -175,24 +162,6 @@ uint64_t lib_clock__get_time_us(void)
 /* ************************************************************************//**
  * \brief	get relative time difference since the given timestamp in milliseconds
  *
- * The returned value may be 0 in the following cases:
- *	1.	The difference really is 0
- *		In that case, one should verify whether this function provides a
- *		sufficient time resolution under all possible circumstances.
- *	2.	The time difference is 49.7 days
- *		In that case, one should verify whether this function provides a
- *		sufficient value range under all possible circumstances.
- *	3.	An error has occurred (provided that _lasttime is also 0)
- *		This may actually only happen if this function is likely to be not
- *		properly supported by the underlying OS.
- *
- * WARNING:
- *	1.	If the actual time difference is greater than 49.7 days, the value of
- *		the difference will overflow, providing an incorrect result.
- *	2.	If _lasttime was provided by a different function than
- *		lib_module_time__get_time(), the result is likely to be invalid, as
- *		the utilized time bases may not be the same.
- *
  * \param	_lasttime			timestamp to calculate the difference from
  * \return	relative time difference in milliseconds
  * ****************************************************************************/
@@ -203,12 +172,6 @@ uint32_t lib_clock__get_time_since_ms(uint32_t _lasttime)
 
 /* ************************************************************************//**
  * \brief delay the calling thread for the given amount of microseconds
- *
- * This function delays the calling thread for _delay microseconds.
- * This function implements a busy wait with an accuracy of 1us.
- * On HOSes, this function implements a thread suspension with an implementation
- * dependent accuracy between 1us and several milliseconds. Furthermore, this
- * function may get prematurely interrupted by a signal on HOSes.
  *
  *  \param _delay : number of microseconds to delay the caller
  * ****************************************************************************/
@@ -231,12 +194,6 @@ void lib_clock__delay_us(uint32_t _delay)
 
 /* ************************************************************************//**
  * \brief	get clock ticks since system startup
- *
- * The CPU time base will overrun after several dozens to several 1000 years,
- * depending on the clock resolution, e.g.:
- *	4679a @  80ns (= 125MHz system clock)
- *	  58a @ 100ps (=  10GHz system clock)
- * Therefore an overrun handling is not really necessary.
  *
  * \return	number of clock ticks as 64bit value
  * ****************************************************************************/
